09862d225d10542b77a94dd056f230ffa4ba2779
Mert Öngengil <mertongngl@hotmail.com>
2018-02-15 22:58:05 +0300

    my modules committed

--- calculator.py
+++ calculator.py
@@ -23,3 +23,37 @@ def division (num_1,num_2):
 def multiplication (num_1,num_2):
 	mul=num_1*num_2
 	return mul
+	  
+def input_module():
+    num1=input("1. Sayiyi Giriniz: ")
+    num2=input("2. Sayiyi Giriniz: ")
+    return num1,num2
+
+def menu_module(num1,num2):
+    menu_str="""
+Islem Secimi:
+
+    1)Toplama Islemi
+    2)Cikarma Islemi
+    3)Carpma Islemi
+    4)Bolme Islemi
+    Cikis icin 'q' harfi girin:
+    """
+    secim=input(menu_str)
+
+    if(secim == "1"):
+        sub(num1,num2)
+    elif(secim == "2"):
+        extraction(num1,num2)
+    elif(secim == "3"):
+        multiplication(num1,num2)
+    elif(secim == "4"):
+        division(num1,num2)
+    elif(secim == "q"):
+        exit()
+    else:
+        pass
+
+
+def output_module(result):
+    print("Sonuc:  "+result)
